c2f15693f53294cccc89f7a8467b7043df2c228d
Youngho Jung <ntyoungho@nate.com>
2023-12-21 15:56:25 +0900

    6nd

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   console.log(os.hostname());
   return (
     <>
-      <h1>홈페이지다22   2번째 버전입니다.!!</h1>
+      <h1>홈페이지다22   6번째 버전입니다.!!</h1>
       <Image src="https://www.shutterstock.com/shutterstock/photos/2095269505/display_1500/stock-photo-lit-up-candle-at-an-outdoor-table-of-a-restaurant-in-winter-cosy-atmosphere-selective-focus-2095269505.jpg" 
         alt="link image"
         priority
